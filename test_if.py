890d0204aa0ec3812a4417b40394e2476f2e5a43
uncoding <foorwork@163.com>
2017-02-15 14:37:20 +0800

    增加判断语句的测试

---
+++ test_if.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+age = raw_input('please input age')
+
+if age > 18:
+	print 'this is a adult man'
+	print 'can do somethings'
+else:
+	print 'this is a child'
+	print 'can not do the works'
